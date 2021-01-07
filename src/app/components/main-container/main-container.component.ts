687f572c4d1bb27d8acea08d6228c53d0e975c6e
Manuel <manuelmiguezg@hotmail.com>
2021-01-07 01:14:40 -0400

    Change in custom material theme colors

--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -6,16 +7,30 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-main-container',
   templateUrl: './main-container.component.html',
-  styleUrls: ['./main-container.component.scss']
+  styleUrls: ['./main-container.component.scss'],
 })
 export class MainContainerComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
+  themeColor: 'primary' | 'accent' | 'warn' = 'primary'; // 👈 notice this
+  isDark = false;
+  constructor(
+    private overlayContainer: OverlayContainer,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    } else {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('dark-theme');
+    }
+  }
 }
